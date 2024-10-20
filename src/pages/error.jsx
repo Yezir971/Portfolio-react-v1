a75874af8a0b427920b5479f5ca4c884d8045ac2
Yezir971 <james_ahmedaly@yahoo.com>
2024-10-20 19:50:44 +0200

    page 404

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,12 +1,22 @@
-import { Typography } from "@material-tailwind/react"
+import { MouseContext } from '@/context/mouseContext'
+import routes from '@/routes'
+import CustomCursor from '@/widgets/cursor/Cursor'
+import { Footer, Navbar } from '@/widgets/layout'
+import { Typography } from '@material-tailwind/react'
+import { useContext } from 'react'
 
-function Error(){
-    return(
-        <>
-
-        <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
+function Error() {
+  let { textEnter, textEnter2, textLeave, textLeave2 } =
+    useContext(MouseContext)
+  return (
+    <>
+      <CustomCursor />
+      <div className="container absolute left-2/4 z-10 mx-auto -translate-x-2/4 p-4">
+        <Navbar routes={routes} />
+      </div>
+      <div className="relative flex h-screen content-center items-center justify-center pt-16 pb-32">
         <div className="absolute top-0 h-full w-full bg-[url('/img/background-3.png')] bg-cover bg-center" />
-        <div className="absolute top-0 h-full w-full bg-black/60 bg-cover bg-center" />
+        <div className="absolute top-0 h-full w-full bg-black bg-cover bg-center" />
         <div className="max-w-8xl container relative mx-auto">
           <div className="flex flex-wrap items-center">
             <div className="ml-auto mr-auto w-full px-4 text-center lg:w-8/12">
@@ -14,19 +24,27 @@ function Error(){
                 variant="h1"
                 color="white"
                 className="mb-6 font-black"
+                onMouseEnter={textEnter}
+                onMouseLeave={textLeave}
               >
                 Page non trovée
               </Typography>
-              <Typography variant="lead" color="white" className="opacity-80">
-                Erreur 404 : La page que vous cherchez n’existe pas
+              <Typography
+                onMouseEnter={textEnter}
+                onMouseLeave={textLeave}
+                variant="lead"
+                color="white"
+                className="opacity-80"
+              >
+                Erreur 404 : La page que vous cherchez n'existe pas ou n'existe plus.
               </Typography>
             </div>
           </div>
         </div>
-        </div>
-        
-        </>
-    )
+      </div>
+      <Footer />
+    </>
+  )
 }
 
 export default Error
